ac0ebe295ab82956c3442a178c06859f337f8b09
Ankurkatri <77335219+Ankurkatri@users.noreply.github.com>
2024-03-25 15:59:08 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -12,6 +12,33 @@ from matplotlib.colors import LinearSegmentedColormap
 from scipy.ndimage import gaussian_filter
 import math
 from collections import Counter
+from wordcloud import WordCloud
+
+
+
+def create_wordcloud(selected_user,df):
+
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    temp['message'] = temp['message'].apply(remove_stop_words)
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
+
 
 
 def set_custom_matplotlib_style():
